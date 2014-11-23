aec2587fa7965554fdf05b9975008d51717298c7
silver-io <silver-io@users.noreply.github.com>
2014-11-23 20:07:42 +0000

    creates restaurant. test pass

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,9 +3,20 @@ require 'rails_helper'
 describe 'restaurants' do
   context 'no restaurants have been added' do
       it 'should display a prompt to add a restaurant' do
-      visit '/restaurants'
-      expect(page).to have_content 'No restaurants'
-      expect(page).to have_link 'Add a restaurant'
+	      visit '/restaurants'
+	      expect(page).to have_content 'No restaurants'
+	      expect(page).to have_link 'Add a restaurant'
+      end
     end
-  end
+
+    context 'restaurants have been added' do
+    	before do
+    		Restaurant.create(name: 'Casa Pepe')
+    	end
+    	it 'should display restaurants' do
+    		visit '/restaurants'
+    		expect(page).to have_content('Casa Pepe')
+    		expect(page).not_to have_content('No restaurants')
+    	end
+    end
 end
