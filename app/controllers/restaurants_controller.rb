31e3bc98addaec5f8b6b3cf2ab566531e23c4aa7
silver-io <silver-io@users.noreply.github.com>
2014-11-23 20:44:20 +0000

    adds form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,5 +3,10 @@ class RestaurantsController < ApplicationController
 	def index
        @restaurants = Restaurant.all
     end
+
+    def new
+    	@restaurant = Restaurant.new
+    end
+
 	
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -20,3 +20,14 @@ describe 'restaurants' do
     	end
     end
 end
+
+describe 'creating restaurants' do
+		 it 'prompts user to fill out a form, then displays the new restaurant' do
+		   visit '/restaurants'
+		   click_link 'Add a restaurant'
+		   fill_in 'Name', with: 'Casa Pepe'
+		   click_button 'Create Restaurant'
+		   expect(page).to have_content 'Casa Pepe'
+		   expect(current_path).to eq '/restaurants'
+        end
+end
