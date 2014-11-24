c368949123da67d5b43177ef0e29ec9a3614f591
silver-io <silver-io@users.noreply.github.com>
2014-11-24 07:09:07 +0000

    creates helper method to so we can use starts on the ratings. First test pass

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,8 @@
 module ReviewsHelper
+
+	def star_rating(rating)
+		return rating unless rating.is_a?(Fixnum)
+        "★" * rating
+	end
+
 end
--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -24,7 +24,7 @@ describe 'reviewing' do
 	it "displays an average rating for all reviews" do
 		leave_review('So so', "3")
         leave_review('Great', "5")
-        expect(page).to have_content("Average rating: 4")
+        expect(page).to have_content("Average rating: ★★★★☆")
 	end
 
 
