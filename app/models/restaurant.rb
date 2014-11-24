877ab42aebeec3a036efc6d59378f14a40dbfa03
silver-io <silver-io@users.noreply.github.com>
2014-11-24 04:06:38 +0000

    ammends error message for when the name is too short

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ActiveRecord::Base
 	has_many :reviews, dependent: :destroy
-	validates :name, length: {minimum: 3}, uniqueness: true
+	validates :name, length: {minimum: 3, message: ": this name is too short, choose another one"}, uniqueness: true
 end
