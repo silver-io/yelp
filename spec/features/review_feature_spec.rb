43c7378614feb8c051a70b0a92a4fe90d205f275
silver-io <silver-io@users.noreply.github.com>
2014-11-24 05:17:16 +0000

    refactors leave_review method

--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -5,13 +5,25 @@ describe 'reviewing' do
 		Restaurant.create(name: 'The Mill')
 	end
 
-	it "allows users to leave reviews using the form that appears alongside restaurants" do
+	def leave_review(thoughts, rating)
 		visit '/restaurants'
 		click_link 'Review The Mill'
-		fill_in "Thoughts", with: "so so"
-		select '3', from: 'Rating'
+		fill_in "Thoughts", with: thoughts
+		select rating, from: 'Rating'
 		click_button 'Leave review'
+	end
+
+
+
+	it "allows users to leave reviews using the form that appears alongside restaurants" do
+		leave_review("so so ", "3")
 		expect(current_path).to eq '/restaurants'
 		expect(page).to have_content 'so so'
 	end
+
+	it "displays an average rating for all reviews" do
+
+	end
+
+
 end
