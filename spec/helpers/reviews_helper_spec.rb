4cd69023681f9fd525e311082134d4d1b0471f1e
silver-io <silver-io@users.noreply.github.com>
2014-11-25 23:46:22 +0000

    completes helper method that adds stars to our reviews. all tests pass

--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe ReviewsHelper, :type => :helper do
 	context '#star_rating'do
-	    it 'does nothing for not a number' do
+	    it 'does nothing if not a number' do
 	    	expect(helper.star_rating('N/A')).to eq "N/A"
 	    end
 
@@ -10,5 +10,12 @@ describe ReviewsHelper, :type => :helper do
 	    	expect(helper.star_rating(5)).to eq '★★★★★'
 	    end
 
+	    it 'returns three black stars and two while stars for three' do
+	    	expect(helper.star_rating(3)).to eq '★★★☆☆'
+	    end
+
+	    it "returns four black stars and one white star for 3.5" do
+        expect(helper.star_rating(3.5)). to eq '★★★★☆'
+        end
 	end
 end
