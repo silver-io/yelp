5c62085a48e879d3099e024932737975c581cfbe
silver-io <silver-io@users.noreply.github.com>
2014-12-01 14:52:51 +0000

    adds test for endorsement feature

---
+++ spec/features/endorsements_feature_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'endorsing reviews' do
+  before do
+    kfc = Restaurant.create(name: 'KFC')
+    kfc.reviews.create(rating: 1, thoughts: "pretty bad")
+  end
+
+  it 'a user can endorse a review, which updates the review endorsement count' do
+    visit '/restaurants'
+    click_link 'Endorse KFC'
+    expect(page).to have_content('1 endorsement')
+  end
+
+end
